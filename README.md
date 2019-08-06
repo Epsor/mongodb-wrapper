00286029daa17b69358c1eab2bf5883ad9631040
Geoffrey Hervet <hervetg@gmail.com>
2019-08-06 14:56:54 +0200

    feat: 🎸 Add requiments to root files

---
+++ README.md
@@ -0,0 +1,5 @@
+# GraphQL API Client for all interfaces.
+
+## Requirements
+
+Both `ENVIRONMENT` and `GRAPHQL_API_FQDN` must be defined as environment variables.
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,23 @@
-const base = require('../../jest.base.config');
-
 const pack = require('./package');
 
 module.exports = {
-  ...base,
+  coveragePathIgnorePatterns: ['node_modules', 'build', 'dist'],
+  coverageReporters: ['json', 'text', 'html'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  verbose: true,
+  moduleFileExtensions: ['js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist'],
+  resetModules: false,
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$',
+  roots: ['<rootDir>/src'],
   displayName: pack.name,
   name: pack.name,
 };
